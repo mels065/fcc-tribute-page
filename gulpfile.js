3457f9fe0c60b883bd87257536e4fbce5ef398d5
Brandon Mellus <mellusbrandon@gmail.com>
2018-09-17 19:01:11 -0400

    Push footer to bottom

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
+const pump = require('pump');
 const browserSync = require('browser-sync').create();
 const path = require('path');
 
 const SRC_DIR = 'src';
 const DEST_DIR = 'static';
 
-gulp.task('serve', ['html', 'scss'], () => {
+gulp.task('serve', ['scss', 'js', 'html'], () => {
   browserSync.init({
     server: {
       baseDir: ['static', 'static/html'],
       routes: {
-        '/css': '../css',
-        '/images': '../images'
+        '/css': './css',
+        '/images': './images',
+        '/js': './js'
       }
     }
   });
 
   gulp.watch(`${SRC_DIR}/sass/**/*.scss`, ['scss']);
+  gulp.watch(`${SRC_DIR}/js/**/*.js`, ['js']);
   gulp.watch(`${SRC_DIR}/html/**/*.html`, ['html']);
 })
 
@@ -34,6 +37,17 @@ gulp.task('scss', () => (
     .pipe(plugins.csso())
     .pipe(gulp.dest(`${DEST_DIR}/css`))
     .pipe(browserSync.stream())
+));
+
+gulp.task('js', () => (
+  pump([
+    gulp.src(`${SRC_DIR}/js/*.js`),
+    plugins.babel({
+      presets: ['@babel/env']
+    }),
+    plugins.uglify(),
+    gulp.dest(`${DEST_DIR}/js`)
+  ])
 ));
 
 gulp.task('default', ['serve']);
